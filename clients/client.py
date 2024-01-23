26cf57152bccc84cb508e05546f6d099c7c8a643
allfantrusd <143310054+allfantrusd@users.noreply.github.com>
2024-01-23 11:48:01 +0530

    Update client.py

--- clients/client.py
+++ clients/client.py
@@ -26,6 +26,6 @@ def client(host: str, port: int):
         client_soc.close()
 
 if __name__ == "__main__":
-    host = "192.168.87.212"
+    host ="server addr"
     port = 8080
     client(host, port)
