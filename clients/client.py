ddec54a1fe20c108368a39391742a1737fc61af6
allfantrusd <143310054+allfantrusd@users.noreply.github.com>
2024-01-25 16:54:05 +0530

    Update client.py

--- clients/client.py
+++ clients/client.py
@@ -1,5 +1,6 @@
 import socket
-import keyboard as key
+import time
+import sys
 
 def client(host: str, port: int):
     client_soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,23 +10,30 @@ def client(host: str, port: int):
 
     try:
         while True:
-            key.wait("g")
-            if not key.is_pressed("g"):
+            time.sleep(2)
+            
+            user_input = input()
+
+            if user_input.lower() == "g":
+                print("\033[91m{}\033[0m".format("Receiving ...."))
                 # Receive the response from the server
                 recv_data = client_soc.recv(1024)
                 print(f"[*] Received data: {recv_data.decode('utf-8')}")
-            else:
-                data = input("Enter the data to send (or type 'exit' to quit): ") or "N/A"
+
+            elif user_input.lower() == "d":
+                data = input("\033[91m{}\033[0m".format("Enter the data to send (or type 'exit' to quit): ")) or "N/A"
                 if data.lower() == "exit":
                     break
                 # Send data to the server
                 client_soc.sendall(data.encode('utf-8'))
             
-            
+            elif user_input.lower() == "exit":
+                break
+
     finally:
         client_soc.close()
 
 if __name__ == "__main__":
-    host ="server addr"
+    host = "server addr"
     port = 8080
     client(host, port)
